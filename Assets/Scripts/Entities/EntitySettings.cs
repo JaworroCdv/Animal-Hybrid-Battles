0a39d40858af2eba3231fbda0d3f1e8b311acbc5
Błażej Jaworski <bjaworski@edu.cdv.pl>
2024-11-09 16:21:51 +0100

    Animal Settings

---
+++ Assets/Scripts/Entities/EntitySettings.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace AnimalHybridBattles.Entities
+{
+    [CreateAssetMenu(menuName = "Data/Animal Settings")]
+    public class EntitySettings : ScriptableObject
+    {
+        [SerializeField] private string entityName;
+        [SerializeField] private Sprite sprite;
+        [SerializeField] private int cost;
+        [SerializeField] private float spawnCooldown;
+        
+        [Header("Stats")]
+        [SerializeField] private int health;
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float attackCooldown;
+        [SerializeField] private float attackDamage;
+
+        public string EntityName => entityName;
+        public Sprite Sprite => sprite;
+        public int Cost => cost;
+        public float SpawnCooldown => spawnCooldown;
+        
+        public int Health => health;
+        public float MovementSpeed => movementSpeed;
+        public float AttackCooldown => attackCooldown;
+        public float AttackDamage => attackDamage;
+    }
+}
